ed21ef4c0bdcf64e8b995b378a91ebea4535077f
yaneurao <yaneurao@gmail.com>
2026-09-06 08:21:40 +0900

    - YOSC : gensfen、実際に使う探索ノード数とMultiPV数を、自動実行・対話モードの両方で生成開始時に   表示するようにした。

--- GenSfen/gensfen.py
+++ GenSfen/gensfen.py
@@ -610,6 +610,26 @@ class GameMatcher:
     def start_games(self):
         """すべての並列対局を開始させる"""
 
+        shared = self.shared
+        if shared.output_format == "hcpe3" and shared.hcpe3_policy_nodes > 0:
+            print_log(
+                "Start GenSfen, "
+                f"OUTPUT_FORMAT = {shared.output_format}, "
+                f"NODES = {shared.nodes}, "
+                "BEST_MULTIPV = 1, "
+                f"HCPE3_POLICY_NODES = {shared.hcpe3_policy_nodes}, "
+                f"HCPE3_POLICY_MULTIPV = {shared.hcpe3_policy_multipv}, "
+                f"MAX_GAME_PLY = {shared.max_game_ply}"
+            )
+        else:
+            print_log(
+                "Start GenSfen, "
+                f"OUTPUT_FORMAT = {shared.output_format}, "
+                f"NODES = {shared.nodes}, "
+                f"MULTIPV = {shared.multipv}, "
+                f"MAX_GAME_PLY = {shared.max_game_ply}"
+            )
+
         # 並列対局数
         num = len(self.engine_threads)
 
@@ -749,24 +769,6 @@ def user_input(argv=None):
             elif i == 'g':
                 # まだ対局が組まれていなければ開始する。
                 if not matcher.shogi_matches:
-                    if shared.output_format == "hcpe3" and shared.hcpe3_policy_nodes > 0:
-                        print_log(
-                            "Start GenSfen, "
-                            f"OUTPUT_FORMAT = {shared.output_format}, "
-                            f"NODES = {shared.nodes}, "
-                            "BEST_MULTIPV = 1, "
-                            f"HCPE3_POLICY_NODES = {shared.hcpe3_policy_nodes}, "
-                            f"HCPE3_POLICY_MULTIPV = {shared.hcpe3_policy_multipv}, "
-                            f"MAX_GAME_PLY = {shared.max_game_ply}"
-                        )
-                    else:
-                        print_log(
-                            "Start GenSfen, "
-                            f"OUTPUT_FORMAT = {shared.output_format}, "
-                            f"NODES = {shared.nodes}, "
-                            f"MULTIPV = {shared.multipv}, "
-                            f"MAX_GAME_PLY = {shared.max_game_ply}"
-                        )
                     matcher.start_games()
 
             elif i == 'q' or i == '!':
